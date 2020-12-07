dd6e20ef42bd8e2b30ef5417439c28958ba2c560
Ecoloqy <r2y.szczepaniak@gmail.com>
2020-12-07 10:45:18 +0100

    Refactor Product object to not hold any information about price and Add getters to the object

--- src/main/java/com/supermarket/Product.java
+++ src/main/java/com/supermarket/Product.java
@@ -1,17 +1,25 @@
 package com.supermarket;
 
-import java.math.BigDecimal;
-
 public class Product {
     private final int barcode;
     private final String name;
     private final int quantity;
-    private final BigDecimal price;
 
-    public Product(int barcode, String name, int quantity, double price) {
+    public Product(int barcode, String name, int quantity) {
         this.barcode = barcode;
         this.name = name;
         this.quantity = quantity;
-        this.price = new BigDecimal(Double.toString(price));
+    }
+
+    public int getBarcode() {
+        return barcode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
     }
 }
