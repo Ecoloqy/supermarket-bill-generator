b266806e3acf652ca4b5b57d58edf83afb747e00
Ecoloqy <r2y.szczepaniak@gmail.com>
2020-12-07 14:16:30 +0100

    Refactor Main class and add more invalid lines to receipt_invalid_test.csv file

--- src/main/java/com/supermarket/Main.java
+++ src/main/java/com/supermarket/Main.java
@@ -6,12 +6,14 @@ import com.supermarket.handlers.InvalidOperationException;
 import java.io.File;
 import java.math.BigDecimal;
 import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.Objects;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InvalidOperationException {
         String promotionsFilename = args.length == 2 ? args[0] : "products.csv";
         String receiptFilename = args.length == 2 ? args[0] : "receipt.csv";
 
@@ -19,13 +21,13 @@ public class Main {
         FilesReader filesReader = new FilesReader();
 
         try {
-            File promotionsFile = Paths.get(Objects.requireNonNull(Main.class.getClassLoader().getResource(promotionsFilename)).toURI()).toFile();
-            File receiptFile = Paths.get(Objects.requireNonNull(Main.class.getClassLoader().getResource(receiptFilename)).toURI()).toFile();
-            List<Promotion> promotions = filesReader.readPromotions(promotionsFile);
-            List<Product> products = filesReader.readProducts(promotionsFile, receiptFile);
+            Path promotionResourcePath = Paths.get(Objects.requireNonNull(Main.class.getClassLoader().getResource(promotionsFilename)).toURI());
+            Path receiptResourcePath = Paths.get(Objects.requireNonNull(Main.class.getClassLoader().getResource(receiptFilename)).toURI());
+            List<Promotion> promotions = filesReader.readPromotions(promotionResourcePath.toFile());
+            List<Product> products = filesReader.readProducts(promotionResourcePath.toFile(), receiptResourcePath.toFile());
             BigDecimal result = billGenerator.generate(products, promotions);
             System.out.println("The total cost of purchases: " + result.toString() + " EUR");
-        } catch (InvalidOperationException | URISyntaxException e) {
+        } catch (InvalidOperationException | URISyntaxException | NullPointerException e) {
             e.printStackTrace();
         }
 
