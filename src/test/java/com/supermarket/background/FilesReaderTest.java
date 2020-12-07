cdb96ee52fac98bd84faa3598954e70035ee0a67
Ecoloqy <r2y.szczepaniak@gmail.com>
2020-12-07 11:24:29 +0100

    Refactor FilesReaderTest class to work with sample test data

--- src/test/java/com/supermarket/background/FilesReaderTest.java
+++ src/test/java/com/supermarket/background/FilesReaderTest.java
@@ -4,12 +4,10 @@ import com.supermarket.Product;
 import com.supermarket.Promotion;
 import com.supermarket.handlers.InvalidOperationException;
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.function.Executable;
 
 import java.io.File;
-import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -17,30 +15,16 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class FilesReaderTest {
-    private static PromotionReader promotionReader;
-    private static ReceiptReader receiptReader;
+    private final PromotionReader promotionReader = new PromotionReader();
+    private final ReceiptReader receiptReader = new ReceiptReader();
     private static List<Promotion> expectedPromotions;
     private static List<Product> expectedProducts;
     private static File promotionsFile;
     private static File productsFile;
-
-    @BeforeAll
-    public static void init() {
-        promotionReader = new PromotionReader();
-        receiptReader = new ReceiptReader();
-        try {
-            promotionsFile = Paths.get(Objects.requireNonNull(FilesReaderTest.class.getClassLoader().getResource("products_test.csv")).toURI()).toFile();
-            productsFile = Paths.get(Objects.requireNonNull(FilesReaderTest.class.getClassLoader().getResource("receipt_test.csv")).toURI()).toFile();
-        } catch (URISyntaxException e) {
-            promotionsFile = null;
-            productsFile = null;
-        }
-        System.out.println("A");
-        System.out.println(FilesReaderTest.class);
-    }
 
     @BeforeEach
     public void beforeEach() {
@@ -49,8 +33,13 @@ public class FilesReaderTest {
     }
 
     @Test
-    public void when_validFileIsProvided_should_returnValidListOfPromotions() {
+    public void when_validFileIsProvided_should_returnValidListOfPromotions() throws InvalidOperationException {
         FilesReader filesReader = new FilesReader(promotionReader, receiptReader);
+        try {
+            promotionsFile = Paths.get(Objects.requireNonNull(FilesReaderTest.class.getClassLoader().getResource("products_test.csv")).toURI()).toFile();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
         expectedPromotions = Arrays.asList(
                 new Promotion(0, 1001, 1, 1.20),
                 new Promotion(1, 1001, 2, 2.00),
@@ -60,19 +49,29 @@ public class FilesReaderTest {
                 new Promotion(5, 1016, 2, 4.00)
         );
 
-        filesReader.readPromotions(promotionsFile);
+        List<Promotion> obtainedPromotions = filesReader.readPromotions(promotionsFile);
+
+        assertIterableEquals(expectedPromotions, obtainedPromotions);
     }
 
     @Test
-    public void when_validFileIsProvided_should_returnValidListOfProducts() {
+    public void when_validFileIsProvided_should_returnValidListOfProducts() throws InvalidOperationException {
         FilesReader filesReader = new FilesReader(promotionReader, receiptReader);
+        try {
+            promotionsFile = Paths.get(Objects.requireNonNull(FilesReaderTest.class.getClassLoader().getResource("products_test.csv")).toURI()).toFile();
+            productsFile = Paths.get(Objects.requireNonNull(FilesReaderTest.class.getClassLoader().getResource("receipt_test.csv")).toURI()).toFile();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
         expectedProducts = Arrays.asList(
                 new Product(1001, "Beer", 3),
                 new Product(1244, "Egg", 8),
                 new Product(1016, "Milk", 3)
         );
 
-        filesReader.readProducts(promotionsFile, productsFile);
+        List<Product> obtainedProducts = filesReader.readProducts(promotionsFile, productsFile);
+
+        assertIterableEquals(expectedProducts, obtainedProducts);
     }
 
     @Test
