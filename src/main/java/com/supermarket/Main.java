f43d566a5739e76bfb011d3d94545649bbd59825
Ecoloqy <r2y.szczepaniak@gmail.com>
2020-12-07 13:18:01 +0100

    Add main static method implementation to initialize all necessary objects and generate result on the screen

--- src/main/java/com/supermarket/Main.java
+++ src/main/java/com/supermarket/Main.java
@@ -1,7 +1,35 @@
 package com.supermarket;
 
+import com.supermarket.background.FilesReader;
+import com.supermarket.handlers.InvalidOperationException;
+
+import java.io.File;
+import java.math.BigDecimal;
+import java.net.URISyntaxException;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Objects;
+
 public class Main {
     public static void main(String[] args) {
-        throw new RuntimeException("Not implemented yet!");
+        String promotionsFilename = args.length == 2 ? args[0] : "products.csv";
+        String receiptFilename = args.length == 2 ? args[0] : "receipt.csv";
+
+        FilesReader filesReader = new FilesReader();
+        List<Promotion> promotions = null;
+        List<Product> products = null;
+
+        try {
+            File promotionsFile = Paths.get(Objects.requireNonNull(Main.class.getClassLoader().getResource(promotionsFilename)).toURI()).toFile();
+            File receiptFile = Paths.get(Objects.requireNonNull(Main.class.getClassLoader().getResource(receiptFilename)).toURI()).toFile();
+            promotions = filesReader.readPromotions(promotionsFile);
+            products = filesReader.readProducts(promotionsFile, receiptFile);
+        } catch (InvalidOperationException | URISyntaxException e) {
+            e.printStackTrace();
+        }
+
+        BillGenerator billGenerator = new BillGenerator();
+        BigDecimal result = billGenerator.generate(products, promotions);
+        System.out.println("The total cost of purchases: " + result.toString());
     }
 }
